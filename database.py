b5a0a0afe59d0fcb06bd0563da5f7b9db19e623f
JestiferHarold <Sanjay.Pandian@tutamail.com>
2025-04-28 14:43:42 +0530

    new file functions added

--- database.py
+++ database.py
@@ -1,4 +1,9 @@
 import mysql.connector as mysql
+from os import sep, path, listdir, remove, mkdir, rmdir
+# from base64 import 
+# from pylocker import ServerLocker
+
+#02121956
 
 database = mysql.connect(
     host = "localhost",
@@ -8,6 +13,8 @@ database = mysql.connect(
 )
 
 cursor = database.cursor()
+
+locker = ServerLocker()
 
 def fetch() -> list:
     try:
@@ -63,7 +70,64 @@ def addUser(username : str, email : str, password : str):
 
 def userLoginPractice(username : str, password : str):
     fetch()
-    mysqlQuery = f"select "
+    mysqlQuery = f"select username, password from accounts where username = '{username}'"
+    cursor.execute()
+    asd = fetch()
+    if asd[1] == password:
+        return True
+    return False
+
+def changeUserName(oldUsername : str, newUsername : str, password : str):
+    fetch()
+    mysqlQuery = f"select username, password from accounts where username = '{oldUsername}'"
+    cursor.execute(mysqlQuery)
+    asd = fetch()
+    if asd[1] == password:
+        cursor.execute(f"update accounts set username = {newUsername} where username = {oldUsername}")
+        return True
+    return False
+
+def createAccountsFolder():
+    if not path.exists("Accounts"):
+        mkdir("Accounts")
+    
+def checkAllAccounts():
+    fetch()
+    cursor.execute("select * from accounts")
+    accounts = fetch()
+    for x in accounts:
+        pass
+
+def createANewAccount(folderName : str):
+    if not path.exists("Accounts" + sep + folderName):
+        mkdir()
+
+def deleteAAccount(folderName : str):
+    if path.exists("Accounts" + sep + folderName):
+       rmdir()
+
+def listAllFiles(fileName : str):
+    if path.exists("Accounts" + sep + fileName):
+        listdir()
+
+def doesFileExists(folderName : str, fileName : str):
+    if fileName + ".txt" in listdir("Accounts" + sep + folderName):
+        return True
+    return False
+
+def createAFile(fileName : str, folderName : str):
+     if not doesFileExists(folderName, fileName):
+        with open("Accounts" + sep + fileName + ".txt") as file:
+            pass
+    
+def createAFileWithContents(fileName : str, folderName : str, contents : str):
+     if not doesFileExists(folderName, fileName):
+        with open("Accounts" + sep + fileName + ".txt") as file:
+            file.write(contents)
+
+def deleteAFile(fileName : str, folderName : str):
+    if doesFileExists(folderName, fileName):
+        remove(folderName + sep + fileName + ".txt")
 
 createTable()
 addUser("asd","asd1","asd2")
